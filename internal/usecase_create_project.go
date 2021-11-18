18286af4996bd62a104c3338fcd207c95c4b2d22
Alexey Golovnya <alexey.golovnya@gmail.com>
2021-11-18 17:33:13 +0700

    #52 can create project check

--- internal/usecase_create_project.go
+++ internal/usecase_create_project.go
@@ -27,7 +27,9 @@ func (u *Apartomat) CreateProject(
 		workspace = workspaces[0]
 	)
 
-	if !u.CanCreateProject(ctx, UserFromCtx(ctx), workspace) {
+	if ok, err := u.CanCreateProject(ctx, UserFromCtx(ctx), workspace); err != nil {
+		return nil, err
+	} else if !ok {
 		return nil, errors.Wrapf(ErrForbidden, "can't create project in workspace (id=%d)", workspace.ID)
 	}
 
@@ -42,6 +44,22 @@ func (u *Apartomat) CreateProject(
 	return u.Projects.Save(ctx, project)
 }
 
-func (u *Apartomat) CanCreateProject(ctx context.Context, subj *UserCtx, obj *store.Workspace) bool {
-	return true
+func (u *Apartomat) CanCreateProject(ctx context.Context, subj *UserCtx, obj *store.Workspace) (bool, error) {
+	if subj == nil {
+		return false, nil
+	}
+
+	wu, err := u.WorkspaceUsers.List(
+		ctx,
+		store.WorkspaceUserStoreQuery{WorkspaceID: expr.IntEq(obj.ID), UserID: expr.IntEq(subj.ID)},
+	)
+	if err != nil {
+		return false, err
+	}
+
+	if len(wu) == 0 {
+		return false, nil
+	}
+
+	return true, nil
 }
