8b3dfec821c5bcf198ab4f0c1e9957aed42dc773
Alexey Golovnya <alexey.golovnya@gmail.com>
2022-04-16 10:24:11 +0700

    #78

---
+++ web-crm/src/Main/Login/useConfirmLoginPin.ts
@@ -0,0 +1,16 @@
+import { FetchResult, useApolloClient } from "@apollo/client"
+import { useConfirmLoginPinMutation, ConfirmLoginPinMutation, ConfirmLoginPinMutationResult } from "../../api/types.d"
+
+export type { ConfirmLoginPinMutation } from "../../api/types.d"
+
+export function useConfirmLoginPin(): [
+    (token: string, pin: string) => Promise<FetchResult<ConfirmLoginPinMutation>>,
+    ConfirmLoginPinMutationResult
+] {
+    const client = useApolloClient()
+    const [ confirmLogin, result ] = useConfirmLoginPinMutation({ client, errorPolicy: 'all' })
+
+    return [(token: string, pin: string) => confirmLogin({ variables: { token, pin } }), result];
+}
+
+export default useConfirmLoginPin;
