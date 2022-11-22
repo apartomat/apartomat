bb267ace1fecac73325d5c941369a7ae5c114812
Alexey Golovnya <alexey.golovnya@gmail.com>
2022-11-22 11:29:05 +0700

    chore: remove expr

--- internal/store/files/store.go
+++ internal/store/files/store.go
@@ -2,23 +2,10 @@ package files
 
 import (
 	"context"
-	"github.com/apartomat/apartomat/internal/pkg/expr"
 )
 
 type Store interface {
 	List(ctx context.Context, spec Spec, limit, offset int) ([]*File, error)
 	Count(context.Context, Spec) (int, error)
 	Save(context.Context, ...*File) error
-}
-
-type ProjectFileStoreQuery struct {
-	ID        expr.Str
-	ProjectID expr.Str
-	Type      ProjectFileTypeExpr
-	Limit     int
-	Offset    int
-}
-
-type ProjectFileTypeExpr struct {
-	Eq []FileType
 }
