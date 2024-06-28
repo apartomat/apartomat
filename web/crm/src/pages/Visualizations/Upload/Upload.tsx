d595189573734233b9ba680675e891a16452bb4a
Alexey Golovnya <alexey.golovnya@gmail.com>
2024-06-28 14:27:18 +0700

    fix(web/crm): visualization upload

---
+++ web/crm/src/pages/Visualizations/Upload/Upload.tsx
@@ -0,0 +1,38 @@
+import { Button } from "grommet"
+import { useState } from "react"
+import { Room, UploadVisualizations } from "features/upload-visualizations"
+
+export function Upload({
+    projectId,
+    rooms,
+    roomId,
+    onVisualizationsUploaded,
+}: {
+    projectId: string
+    rooms: { id: string; name: string }[]
+    roomId?: string
+    onVisualizationsUploaded?: ({ files }: { files: File[] }) => void
+}) {
+    const [showUploadForm, setShowUploadForm] = useState(false)
+
+    return (
+        <>
+            <Button label="Загрузить" onClick={() => setShowUploadForm(true)} />
+
+            {showUploadForm && (
+                <UploadVisualizations
+                    projectId={projectId}
+                    rooms={rooms}
+                    roomId={roomId}
+                    onClickClose={() => setShowUploadForm(false)}
+                    onClickOutside={() => setShowUploadForm(false)}
+                    onEsc={() => setShowUploadForm(false)}
+                    onVisualizationsUploaded={(files) => {
+                        setShowUploadForm(false)
+                        onVisualizationsUploaded && onVisualizationsUploaded(files)
+                    }}
+                />
+            )}
+        </>
+    )
+}
